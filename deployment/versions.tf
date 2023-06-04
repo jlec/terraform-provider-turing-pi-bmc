6ed81a42c17776d80ffbe91f69ae535f7012ec9c
Justin Lecher <justin@jlec.de>
2023-06-04 20:18:38 +0100

    refactor: Migrate TFE backend

--- deployment/versions.tf
+++ deployment/versions.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.4.0"
   cloud {
-    organization = "jlec"
+    organization = "jlec-devops"
 
     workspaces {
       name = "terraform_provider_turing_pi_bmc"
